5c0e023bf04be6ac8fa9147fa09f70b86074af13
bagasprimanto <39642138+bagasprimanto@users.noreply.github.com>
2024-07-04 00:37:45 +0100

    Finished About page

--- pages/urls.py
+++ pages/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", HomePageView.as_view(), name="home"),
     path("about/", AboutPageView.as_view(), name="about"),
-    path("feedback/", FeedbackPageView.as_view(), name="about"),
+    path("feedback/", FeedbackPageView.as_view(), name="feedback"),
 ]
