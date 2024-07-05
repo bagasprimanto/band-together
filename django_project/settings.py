2889dbf88404cf0d2d912382338d9e94b055efa6
bagasprimanto <39642138+bagasprimanto@users.noreply.github.com>
2024-07-05 09:13:06 +0100

    Added website name and address to emails

--- django_project/settings.py
+++ django_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",
     # 3rd party
     # Crispy Forms
     "crispy_forms",
