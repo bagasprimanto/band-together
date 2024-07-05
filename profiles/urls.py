b8b55e74a450642a349c20dbd84289efa7ca8489
bagasprimanto <39642138+bagasprimanto@users.noreply.github.com>
2024-07-05 13:23:36 +0100

    Added logic in header for checking if the user has a profile or not

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import ProfileListView, ProfileDetailView
+from .views import ProfileCreateView, ProfileListView, ProfileDetailView
 
 app_name = "profiles"
 urlpatterns = [
     path("", ProfileListView.as_view(), name="profile_list"),
+    path("new/", ProfileCreateView.as_view(), name="profile_new"),
     path("detail/", ProfileDetailView.as_view(), name="profile_detail"),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,36 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.urls import reverse, reverse_lazy
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
 
 
-# Create your views here.
+from .models import Profile
+from .forms import ProfileForm
+
+
+class ProfileCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = Profile
+    form_class = ProfileForm
+    template_name = "profiles/profile_form.html"
+    success_url = reverse_lazy("profile_detail")
+
+    def test_func(self):
+        # This function checks if the user already has a profile
+        return not Profile.objects.filter(user=self.request.user).exists()
+
+    def handle_no_permission(self):
+        # Redirect to the profile detail page if the user already has a profile
+        if self.request.user.is_authenticated:
+            return redirect("profile_detail", slug=self.request.user.profile.slug)
+        return super().handle_no_permission()
+
+    def form_valid(self, form):
+        # sets the user instance of the Profile to the user creating the profile
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
 class ProfileListView(TemplateView):
     template_name = "profiles/profile_list.html"
 
