31cbab95234873e4970e87685afdda1d557f2cc8
bagasprimanto <39642138+bagasprimanto@users.noreply.github.com>
2024-07-06 00:22:17 +0100

    Displayed profile picture overlapping cover picture in Profile Detail

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from django.urls import path
-from .views import ProfileCreateView, ProfileListView, ProfileDetailView, MessageUser
+from .views import (
+    ProfileCreateView,
+    ProfileListView,
+    ProfileDetailView,
+    MessageUser,
+    ProfileEditView,
+)
 
 app_name = "profiles"
 urlpatterns = [
     path("", ProfileListView.as_view(), name="profile_list"),
     path("new/", ProfileCreateView.as_view(), name="profile_new"),
-    path("detail/<slug:slug>/", ProfileDetailView.as_view(), name="profile_detail"),
+    path("<slug:slug>/", ProfileDetailView.as_view(), name="profile_detail"),
+    path("<slug:slug>/edit", ProfileEditView.as_view(), name="profile_edit"),
     path("chat/", MessageUser.as_view(), name="message_user"),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
-from django.views.generic import TemplateView, CreateView, DetailView
+from django.views.generic import TemplateView, CreateView, DetailView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from .mixins import ProfileRequiredMixin
@@ -53,5 +53,21 @@ class ProfileDetailView(DetailView):
         return context
 
 
+class ProfileEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Profile
+    fields = "__all__"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        profile = self.get_object()
+        if self.request.user == profile.user:
+            return True
+        return False
+
+
+## JUST FOR TESTING
 class MessageUser(LoginRequiredMixin, ProfileRequiredMixin, TemplateView):
     template = "profiles/messaging.html"
