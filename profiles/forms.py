a6f5aadc72ccd963553232db02fbe5324cf61d93
bagasprimanto <39642138+bagasprimanto@users.noreply.github.com>
2024-07-07 12:28:10 +0100

    Added genres to edit profile tabs

--- profiles/forms.py
+++ profiles/forms.py
@@ -174,3 +174,47 @@ class ProfileEditPicturesForm(forms.ModelForm):
             "profile_picture",
             "cover_picture",
         ]
+
+
+class ProfileEditSkillsForm(forms.ModelForm):
+
+    profile_picture = forms.ImageField(
+        label="Add a profile picture",
+        required=False,
+    )
+
+    cover_picture = forms.ImageField(
+        label="Add a cover picture",
+        required=False,
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(ProfileEditPicturesForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)  # Create an instance of the FormHelper class
+        self.helper.form_id = "profile-picture-form"
+        self.helper.form_method = "POST"
+
+        # Pass in the profile_url variable
+        if "instance" in kwargs and kwargs["instance"] is not None:
+            profile_url = kwargs["instance"].get_absolute_url()
+        else:
+            profile_url = "#"  # Default to # if no instance provided
+
+        self.helper.layout = Layout(
+            "profile_picture",
+            "cover_picture",
+            Submit("submit", "Submit", css_class="btn btn-primary"),
+            Button(
+                "cancel",
+                "Cancel",
+                css_class="btn btn-secondary",
+                onclick=f"window.location.href='{profile_url}'",
+            ),
+        )
+
+    class Meta:
+        model = Profile
+        fields = [
+            "profile_picture",
+            "cover_picture",
+        ]
