78690d0ff56fee39745d67fb56cc319f7cf64aaa
Demetrius Bagas Primanto <39642138+bagasprimanto@users.noreply.github.com>
2024-07-12 20:29:26 +0100

    Create new message using function-based-views

--- inbox/urls.py
+++ inbox/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("", InboxView.as_view(), name="inbox"),
     path("<int:pk>/", InboxDetailView.as_view(), name="inbox_detail"),
     path("search-profiles/", search_profiles, name="inbox_searchprofiles"),
-    path("create-message/<slug:slug>", create_message, name="inbox_createmessage"),
+    path("create-message/<slug:slug>/", create_message, name="inbox_createmessage"),
 ]
--- inbox/views.py
+++ inbox/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Conversation, InboxMessage
 from profiles.models import Profile
 from django.views.generic import ListView, DetailView
@@ -6,6 +6,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from profiles.mixins import ProfileRequiredMixin
 from django.http import HttpResponse, Http404
 from .forms import InboxCreateMessageForm
+from django.utils import timezone
 
 
 class InboxView(LoginRequiredMixin, ProfileRequiredMixin, ListView):
@@ -53,8 +54,30 @@ def search_profiles(request):
 def create_message(request, slug):
     recipient = get_object_or_404(Profile, slug=slug)
     create_message_form = InboxCreateMessageForm()
+
+    if request.method == "POST":
+        form = InboxCreateMessageForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.sender = request.user.profile
+
+            my_conversations = request.user.profile.conversations.all()
+            for c in my_conversations:
+                if recipient in c.participants.all():
+                    message.conversation = c
+                    message.save()
+                    c.lastmessage_created = timezone.now()
+                    c.save()
+                    return redirect("inbox:inbox_detail", pk=c.pk)
+            new_conversation = Conversation.objects.create()
+            new_conversation.participants.add(request.user.profile, recipient)
+            new_conversation.save()
+            message.conversation = new_conversation
+            message.save()
+            return redirect("inbox:inbox_detail", pk=c.pk)
+
     context = {
         "recipient": recipient,
-        "create_message_form": create_message_form,
+        "form": create_message_form,
     }
     return render(request, "inbox/createmessage_form.html", context)
