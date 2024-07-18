40ce1b6401f8c71b4139afcd2701dd7a819bfe44
Demetrius Bagas Primanto <39642138+bagasprimanto@users.noreply.github.com>
2024-07-18 11:05:05 +0100

    Changed import code for importing settings

--- advertisements/views.py
+++ advertisements/views.py
@@ -17,7 +17,7 @@ from profiles.mixins import ProfileRequiredMixin
 from django.http import HttpResponseRedirect
 from django.core.paginator import Paginator
 from django.http import Http404
-from django_project import settings
+from django.conf import settings
 
 
 def advertisement_list(request):
--- profiles/views.py
+++ profiles/views.py
@@ -22,7 +22,7 @@ from advertisements.models import Advertisement
 from inbox.forms import InboxCreateMessageForm
 from .filters import ProfileFilter
 from django.core.paginator import Paginator
-from django_project import settings
+from django.conf import settings
 
 
 class ProfileCreateView(
