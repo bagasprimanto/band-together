7218cbe6c8275fda3ddb51a92f35d7e105c9d85d
Demetrius Bagas Primanto <39642138+bagasprimanto@users.noreply.github.com>
2024-07-25 11:10:18 +0100

    Remove unused accounts sign up views, and add tests to accounts and bookmarks

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,1 @@
-from django.urls import path
-
-from .views import SignupPageView
-
-urlpatterns = [
-    path("signup/", SignupPageView.as_view(), name="signup"),
-]
+# no urls required, logics handled by django-allauth
