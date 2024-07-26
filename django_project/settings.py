185919f60f78ca163b07d6474e0bd69a18f47cfc
Demetrius Bagas Primanto <39642138+bagasprimanto@users.noreply.github.com>
2024-07-26 11:09:10 +0100

    Moved environ import to the top of the settings.py file and corrected static url to cover_pic_default.jpg in _profile_deta_base.html

--- django_project/settings.py
+++ django_project/settings.py
@@ -10,19 +10,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
-
-# from environs import Env
 import environ
-import os
+from pathlib import Path
 
 from django.contrib import messages
 
-env = environ.Env()
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-environ.Env.read_env(env_file=os.path.join(BASE_DIR, ".env"))
+environ.Env.read_env(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
