59723011f4f5dcff66d12fb0f66d900a42d48b7e
Demetrius Bagas Primanto <39642138+bagasprimanto@users.noreply.github.com>
2024-07-26 12:42:03 +0100

    Extracted javascript into separate files

--- profiles/views.py
+++ profiles/views.py
@@ -163,6 +163,7 @@ class ProfileAdsDetailView(BookmarkSingleObjectMixin, BookmarkMixin, DetailView)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["create_message_form"] = InboxCreateMessageForm()
 
         # Get InboxCreateMessageForm
         context["form"] = InboxCreateMessageForm()
--- staticfiles/js/profiles/profile_detail/youtube_modal.js
+++ staticfiles/js/profiles/profile_detail/youtube_modal.js
@@ -15,14 +15,3 @@ videoModal.addEventListener('hidden.bs.modal', function (event) {
     var videoFrame = videoModal.querySelector('#videoFrame')
     videoFrame.src = ''
 })
-
-// For dismissing the create message modal when cancel button is clicked
-document.addEventListener('DOMContentLoaded', function () {
-    var cancelButton = document.getElementById('cancel-button');
-    if (cancelButton) {
-        cancelButton.addEventListener('click', function () {
-            var createMessageModal = bootstrap.Modal.getInstance(document.getElementById('createMessageModal'));
-            createMessageModal.hide();
-        });
-    }
-});
