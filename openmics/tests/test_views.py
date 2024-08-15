a1276a8cb12d2b6cceaf6882357c4e3c433d8e79
Demetrius Bagas Primanto <39642138+bagasprimanto@users.noreply.github.com>
2024-08-15 22:25:47 +0100

    Add tests to openmics views

--- openmics/tests/test_views.py
+++ openmics/tests/test_views.py
@@ -21,6 +21,7 @@ class OpenMicViewsTests(TestCase):
             start_time=time(19, 0),
             end_time=time(23, 0),
             author=self.profile,
+            google_maps_link="https://maps.google.com/?q=37.7749,-122.4194",
         )
         self.openmic2 = OpenMic.objects.create(
             title="Open Mic 2",
@@ -28,6 +29,7 @@ class OpenMicViewsTests(TestCase):
             start_time=time(19, 0),
             end_time=time(23, 0),
             author=self.profile,
+            google_maps_link="https://maps.google.com/?q=37.7749,-122.4194",
         )
 
     def test_openmic_list_view_no_filter(self):
@@ -52,91 +54,95 @@ class OpenMicViewsTests(TestCase):
         response = self.client.get(
             reverse("openmics:get_openmics"), {"page": 1}, HTTP_HX_REQUEST="true"
         )
+
+        # Ensure the resopnse code is 200
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(
-            response, "openmics/openmic_list_partial.html#openmics_list"
-        )
+
+        # Ensure the response contains the title of the Open Mic event
         self.assertContains(response, self.openmic1.title)
 
-    # def test_get_openmics_partial_view_no_htmx(self):
-    #     response = self.client.get(reverse("openmics:get_openmics"), {"page": 1})
-    #     self.assertEqual(response.status_code, 400)
-    #     self.assertContains(response, "This endpoint only accepts HTMX requests.")
+    def test_get_openmics_partial_view_no_htmx(self):
+        response = self.client.get(reverse("openmics:get_openmics"), {"page": 1})
+        self.assertEqual(response.status_code, 400)
+        self.assertIn(
+            "This endpoint only accepts HTMX requests.", response.content.decode()
+        )
 
-    # def test_openmic_detail_view(self):
-    #     response = self.client.get(
-    #         reverse("openmics:openmic_detail", kwargs={"pk": self.openmic1.pk})
-    #     )
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, "openmics/openmic_detail.html")
-    #     self.assertContains(response, self.openmic1.title)
-    #     self.assertIn("comment_form", response.context)
-    #     self.assertIn("comments", response.context)
-    #     self.assertIn("report_form", response.context)
+    def test_openmic_detail_view(self):
+        response = self.client.get(
+            reverse("openmics:openmic_detail", kwargs={"pk": self.openmic1.pk})
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "openmics/openmic_detail.html")
+        self.assertContains(response, self.openmic1.title)
+        self.assertIn("comment_form", response.context)
+        self.assertIn("comments", response.context)
+        self.assertIn("report_form", response.context)
 
-    # def test_openmic_detail_view_invalid_google_maps_url(self):
-    #     self.openmic1.google_maps_link = "invalid_url"
-    #     self.openmic1.save()
-    #     response = self.client.get(
-    #         reverse("openmics:openmic_detail", kwargs={"pk": self.openmic1.pk})
-    #     )
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertIn("error", response.context)
-    #     self.assertEqual(response.context["error"], "Invalid Google Maps URL.")
+    def test_openmic_detail_view_invalid_google_maps_url(self):
+        self.openmic1.google_maps_link = "invalid_url"
+        self.openmic1.save()
+        response = self.client.get(
+            reverse("openmics:openmic_detail", kwargs={"pk": self.openmic1.pk})
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("error", response.context)
+        self.assertEqual(response.context["error"], "Invalid Google Maps URL.")
 
-    # def test_comment_create_view_valid(self):
-    #     self.client.login(username="testuser", password="password")
-    #     response = self.client.post(
-    #         reverse("openmics:add_comment", kwargs={"pk": self.openmic1.pk}),
-    #         {"body": "This is a test comment."},
-    #     )
-    #     self.assertEqual(response.status_code, 302)
-    #     self.assertRedirects(
-    #         response,
-    #         reverse("openmics:openmic_detail", kwargs={"pk": self.openmic1.pk}),
-    #     )
-    #     self.assertTrue(
-    #         Comment.objects.filter(
-    #             body="This is a test comment.", parent_openmic=self.openmic1
-    #         ).exists()
-    #     )
+    def test_comment_create_view_valid(self):
+        self.client.login(username="testuser", password="password")
+        response = self.client.post(
+            reverse("openmics:comment_create", kwargs={"pk": self.openmic1.pk}),
+            {"body": "This is a test comment."},
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(
+            response,
+            reverse("openmics:openmic_detail", kwargs={"pk": self.openmic1.pk}),
+        )
+        self.assertTrue(
+            Comment.objects.filter(
+                body="This is a test comment.", parent_openmic=self.openmic1
+            ).exists()
+        )
 
-    # def test_comment_create_view_invalid(self):
-    #     self.client.login(username="testuser", password="password")
-    #     response = self.client.post(
-    #         reverse("openmics:add_comment", kwargs={"pk": self.openmic1.pk}), {}
-    #     )
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, "openmics/openmic_detail.html")
-    #     self.assertFalse(Comment.objects.filter(parent_openmic=self.openmic1).exists())
-    #     self.assertContains(response, "This field is required.")
+    def test_comment_create_view_invalid(self):
+        self.client.login(username="testuser", password="password")
+        response = self.client.post(
+            reverse("openmics:comment_create", kwargs={"pk": self.openmic1.pk}),
+            {"body": ""},  # Simulate submitting an empty comment
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "openmics/openmic_detail.html")
+        self.assertFalse(Comment.objects.filter(parent_openmic=self.openmic1).exists())
+        self.assertIn("required", response.content.decode())
 
-    # def test_comment_delete_view_valid(self):
-    #     self.client.login(username="testuser", password="password")
-    #     comment = Comment.objects.create(
-    #         author=self.profile, parent_openmic=self.openmic1, body="A test comment."
-    #     )
-    #     response = self.client.post(
-    #         reverse("openmics:delete_comment", kwargs={"pk": comment.pk})
-    #     )
-    #     self.assertEqual(response.status_code, 302)
-    #     self.assertRedirects(
-    #         response,
-    #         reverse("openmics:openmic_detail", kwargs={"pk": self.openmic1.pk}),
-    #     )
-    #     self.assertFalse(Comment.objects.filter(pk=comment.pk).exists())
+    def test_comment_delete_view_valid(self):
+        self.client.login(username="testuser", password="password")
+        comment = Comment.objects.create(
+            author=self.profile, parent_openmic=self.openmic1, body="A test comment."
+        )
+        response = self.client.post(
+            reverse("openmics:comment_delete", kwargs={"pk": comment.pk})
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(
+            response,
+            reverse("openmics:openmic_detail", kwargs={"pk": self.openmic1.pk}),
+        )
+        self.assertFalse(Comment.objects.filter(pk=comment.pk).exists())
 
-    # def test_comment_delete_view_invalid_user(self):
-    #     other_user = User.objects.create_user(username="otheruser", password="password")
-    #     other_profile = Profile.objects.create(
-    #         user=other_user, display_name="Other User"
-    #     )
-    #     comment = Comment.objects.create(
-    #         author=other_profile, parent_openmic=self.openmic1, body="A test comment."
-    #     )
-    #     self.client.login(username="testuser", password="password")
-    #     response = self.client.post(
-    #         reverse("openmics:delete_comment", kwargs={"pk": comment.pk})
-    #     )
-    #     self.assertEqual(response.status_code, 403)
-    #     self.assertTrue(Comment.objects.filter(pk=comment.pk).exists())
+    def test_comment_delete_view_invalid_user(self):
+        other_user = User.objects.create_user(username="otheruser", password="password")
+        other_profile = Profile.objects.create(
+            user=other_user, display_name="Other User"
+        )
+        comment = Comment.objects.create(
+            author=other_profile, parent_openmic=self.openmic1, body="A test comment."
+        )
+        self.client.login(username="testuser", password="password")
+        response = self.client.post(
+            reverse("openmics:comment_delete", kwargs={"pk": comment.pk})
+        )
+        self.assertEqual(response.status_code, 403)
+        self.assertTrue(Comment.objects.filter(pk=comment.pk).exists())
