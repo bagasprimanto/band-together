95a64e6b0bd81f85e51bdc5a7de408cf4ec713bd
Demetrius Bagas Primanto <39642138+bagasprimanto@users.noreply.github.com>
2024-08-27 20:39:51 +0100

    Add comments to advertisements

--- advertisements/views.py
+++ advertisements/views.py
@@ -9,7 +9,6 @@ from django.views.generic import (
     DeleteView,
 )
 from .models import Advertisement, Comment
-from profiles.models import Profile
 from .forms import AdvertisementCreateForm, AdvertisementEditForm, CommentCreateForm
 from inbox.forms import InboxCreateMessageForm
 from .filters import AdvertisementFilter
@@ -67,34 +66,41 @@ def advertisement_list(request):
 
 
 def get_advertisements(request):
+    """
+    Handles HTMX requests to get advertisements for pages beyond the first page.
+    """
+
+    # Check if the request is an HTMX request. If not, raise a 404 error.
     if not request.headers.get("HX-Request"):
         raise Http404()
 
+    # Get the page number from the GET parameters. If not provided, default to page 1.
     page = request.GET.get(
-        "page", 1
-    )  # ?page=2, then this will extract 2. If it doesn't, then default to 1
+        "page", 1  # ?page=2 will extract 2; defaults to 1 if not present.
+    )
 
+    # Initialize the advertisement filter with the GET parameters and the queryset of all advertisements,
+    # ordered by the last updated date (descending).
     f = AdvertisementFilter(
         request.GET, queryset=Advertisement.objects.all().order_by("-last_updated")
     )
+
+    # Check if any filter fields are present in the GET parameters.
     has_filter = any(field in request.GET for field in set(f.get_fields()))
 
+    # If no filters are applied, retrieve all advertisements ordered by the last updated date.
+    # If filters are applied, get the filtered queryset.
     if not has_filter:
         advertisements = Advertisement.objects.all().order_by("-last_updated")
     else:
         advertisements = f.qs
 
-    # If the HTMX request is coming from the profile detail page, get and filter by the profile_slug from the request
-    profile_slug = request.GET.get("profile_slug", None)
-
-    if profile_slug:
-        profile = get_object_or_404(Profile, slug=profile_slug)
-        advertisements = advertisements.filter(author=profile)
-
+    # Paginate the advertisements with the number of items per page defined in settings.PAGE_SIZE.
     paginator = Paginator(advertisements, settings.PAGE_SIZE)
     context = {"ads": paginator.page(page)}
 
-    # Add bookmark context
+    # Add bookmark context to the context dictionary.
+    # Gets bookmarks for advertisements pertaining to the current user.
     bookmark_context = BookmarkMixin().get_bookmark_context(
         request.user, advertisements
     )
@@ -110,18 +116,33 @@ def get_advertisements(request):
 class AdvertisementCreateView(
     LoginRequiredMixin, ProfileRequiredMixin, SuccessMessageMixin, CreateView
 ):
+    """
+    View for creating new Advertisements
+    """
+
+    # The model that this view will operate on.
     model = Advertisement
+
+    # The form class that will be used to create a new advertisement.
     form_class = AdvertisementCreateForm
+
+    # The success message to display when an advertisement is successfully created.
     success_message = "Successfully created ad!"
+
+    # The template used to render the form.
     template_name = "advertisements/advertisement_form.html"
 
     def form_valid(self, form):
-        # sets the author instance of the Profile to the user creating the profile
+        # This method is called when the submitted form is valid.
+        # Set the author of the advertisement to the current user's profile.
         form.instance.author = self.request.user.profile
+
+        # Call the superclass's form_valid method to save the form and handle the redirection.
         return super().form_valid(form)
 
     def get_success_url(self):
-        # Returns the URL to redirect to after the form is successfully submitted
+        # This method returns the URL to redirect to after the advertisement is successfully created.
+        # The URL will point to the detail view of the newly created advertisement.
         return reverse(
             "advertisements:advertisement_detail", kwargs={"pk": self.object.pk}
         )
