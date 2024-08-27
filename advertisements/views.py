f4a6ea33c16204571dc74d2358dc60284caeba9a
Demetrius Bagas Primanto <39642138+bagasprimanto@users.noreply.github.com>
2024-08-27 20:14:13 +0100

    Add comments for advertisement_list

--- advertisements/views.py
+++ advertisements/views.py
@@ -23,32 +23,46 @@ from reports.forms import ReportForm
 
 
 def advertisement_list(request):
+    """
+    Gets the first page of the list of advertisements
+    """
+
+    # Part of Django filters,
+    # initialize the advertisement filter with the GET parameters and the queryset of all advertisements,
+    # ordered by the last updated date (descending).
     f = AdvertisementFilter(
         request.GET, queryset=Advertisement.objects.all().order_by("-last_updated")
     )
+
+    # Check if any filter fields are present in the GET parameters.
     has_filter = any(field in request.GET for field in set(f.get_fields()))
 
+    # If no filters are applied, retrieve all advertisements ordered by the last updated date.
     if not has_filter:
         advertisements = Advertisement.objects.all().order_by("-last_updated")
     else:
+        # If filters are applied, get the filtered queryset.
         advertisements = f.qs
 
+    # Paginate the advertisements with the number of items per page defined in settings.PAGE_SIZE.
+    # This defaults to the first page when this view is triggered.
     paginator = Paginator(advertisements, settings.PAGE_SIZE)
     advertisements_page = paginator.page(1)  # default to 1 when this view is triggered
 
     context = {
-        "form": f.form,
+        "form": f.form,  # The form object associated with the filter.
         "ads": advertisements_page,
-        "ads_count": advertisements.count,
+        "ads_count": advertisements.count,  # The total count of advertisements.
         "has_filter": has_filter,
     }
 
-    # Add bookmark context
+    # Add bookmark context to the context dictionary.
     bookmark_context = BookmarkMixin().get_bookmark_context(
         request.user, advertisements
     )
-    context.update(bookmark_context)
+    context.update(bookmark_context)  # Update the context with bookmark information.
 
+    # Render the advertisement list template with the provided context.
     return render(request, "advertisements/advertisement_list.html", context)
 
 
