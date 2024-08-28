52cc347582afa14c33213dbd1ccf1712cb66c12c
Demetrius Bagas Primanto <39642138+bagasprimanto@users.noreply.github.com>
2024-08-28 11:13:58 +0100

    Add comments to inbox admin

--- inbox/admin.py
+++ inbox/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import InboxMessage, Conversation
 
+# Register the InboxMessage model with the Django admin site.
 admin.site.register(InboxMessage)
+# Register the Conversation model with the Django admin site.
 admin.site.register(Conversation)
