ff04b4931210d55bcb5e21ad536379cef7372fd2
Demetrius Bagas Primanto <39642138+bagasprimanto@users.noreply.github.com>
2024-08-28 10:22:47 +0100

    Add comments to open mic forms

--- openmics/forms.py
+++ openmics/forms.py
@@ -4,6 +4,10 @@ from dal import autocomplete
 
 
 class OpenMicCreateForm(forms.ModelForm):
+    """
+    This form is used to create an open mic. It is used inside the Django admin interface.
+    """
+
     class Meta:
         model = OpenMic
         fields = "__all__"
@@ -13,6 +17,9 @@ class OpenMicCreateForm(forms.ModelForm):
 
 
 class CommentCreateForm(forms.ModelForm):
+    """
+    This form is used to create a comment in an open mic.
+    """
 
     body = forms.CharField(
         max_length=150,
